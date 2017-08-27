c091d3db8cc59265ea76bc3d01463607d108242c
emceelamb <mlam@berkeley.edu>
2017-08-27 06:42:23 -0700

    I blew it up by fixed it :sad: ;_;

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@5o!0#x^xq!3@&d_75o*d#=^4@0^s!8$oox6-mb@vl#7+67c8@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.toolittletoolate.markofthelam.com']
 
