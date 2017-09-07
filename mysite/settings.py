3f4cfa1c06d905c4e3f2e68e650bfb9451fe297b
emceelamb <mlam@berkeley.edu>
2017-09-07 13:25:47 -0700

    nuked ga

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,12 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'sorry',
     'google_analytics',
 ]
 
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
